3d72806cbd6b220b1f5fafde5dd64fa398d3bae4
Andrew Wirick <awirick@gmail.com>
2016-10-24 17:14:02 -0500

    manual memrge of latest supercollider, keeping aviture parsing code.

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 function Supercollider() {
   this.options = {};
+  this.searchOptions = {
+    extra: [],
+    sort: [],
+    pageTypes: {}
+  };
   this.adapters = {};
   this.tree = [];
   this.template = null;
@@ -10,5 +15,8 @@ Supercollider.prototype.parse = require('./lib/parse');
 Supercollider.prototype.build = require('./lib/build');
 Supercollider.prototype.adapter = require('./lib/adapter');
 Supercollider.prototype.config = require('./lib/config');
+Supercollider.prototype.buildSearch = require('./lib/buildSearch');
+Supercollider.prototype.searchConfig = require('./lib/searchConfig');
 
 module.exports = new Supercollider();
+module.exports.Supercollider = Supercollider;
